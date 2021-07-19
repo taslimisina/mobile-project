c16de35c3bbba9eda065a3f0e5b88aaa4299de48
ShadiGhasemi <shadighasemi.sg@gmail.com>
2021-07-19 11:55:57 +0430

    checklist layout

--- app/src/main/java/ir/sharif/mobile/project/ui/habits/HabitEditFragment.java
+++ app/src/main/java/ir/sharif/mobile/project/ui/habits/HabitEditFragment.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,6 +26,6 @@ public class HabitEditFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_habit_edit, container, false);
+        return inflater.inflate(R.layout.fragment_edit_habit, container, false);
     }
 }
