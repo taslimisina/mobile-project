69816a2af5b2d7ac39d780aee90bf5623d70e5fd
ShadiGhasemi <shadighasemi.sg@gmail.com>
2021-07-19 16:56:05 +0430

    merge edit habit

--- app/src/main/java/ir/sharif/mobile/project/ui/habits/HabitEditFragment.java
+++ app/src/main/java/ir/sharif/mobile/project/ui/habits/HabitEditFragment.java
@@ -30,7 +30,7 @@ public class HabitEditFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_habit_edit, container, false);
+        View view = inflater.inflate(R.layout.fragment_edit_habit, container, false);
 //        view.findViewById(R.id.upButton).setOnClickListener();    // Todo set on click listener (navigate back)
         return view;
     }
