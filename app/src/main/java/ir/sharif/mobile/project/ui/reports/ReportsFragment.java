5a1f0bf39a227156a4ebbbf846b581dbdcbe35f0
seyedalireza <sarhosseini99@gmail.com>
2021-07-23 00:48:16 +0430

    add some features to chart

--- app/src/main/java/ir/sharif/mobile/project/ui/reports/ReportsFragment.java
+++ app/src/main/java/ir/sharif/mobile/project/ui/reports/ReportsFragment.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.github.mikephil.charting.charts.LineChart;
@@ -25,6 +26,8 @@ import java.util.Map;
 import ir.sharif.mobile.project.R;
 import ir.sharif.mobile.project.ui.repository.RepositoryHolder;
 
+import static android.view.View.INVISIBLE;
+
 public class ReportsFragment extends Fragment {
 
     private ReportsViewModel reportsViewModel;
@@ -36,28 +39,23 @@ public class ReportsFragment extends Fragment {
                 new ViewModelProvider(this).get(ReportsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_reports, container, false);
         chart = root.findViewById(R.id.line_chart);
-        chart.setTouchEnabled(true);
-        chart.setDragEnabled(true);
-        chart.setScaleEnabled(true);
-        chart.setPinchZoom(true);
-
-        chart.getXAxis().setTextSize(50f);
-        chart.getAxisLeft().setTextSize(50f);
-        Legend l = chart.getLegend();
-        l.setForm(Legend.LegendForm.LINE);
-        Map<Date, Integer> history = RepositoryHolder.getCoinRepository().getHistory();
-        List<Date> dates = new ArrayList<>(history.keySet());
-
-        Collections.sort(dates, (o1, o2) -> o1.after(o2) ? 1 : o1.equals(o2) ? 0 : -1);
-        List<String> labels = new ArrayList<>();
-        List<Entry> entries = new ArrayList<>();
-        for (int i = 0; i < Math.min(20, dates.size()); i++) {
-            Date date = dates.get(i);
-            entries.add(new Entry(history.get(date), i));
-            labels.add(date.getMonth() + "/" + date.getDay() + " " + date.getHours() + ":" + date.getMinutes());
-        }
-        chart.setData(new LineData(labels, new LineDataSet(entries, "score")));
-        chart.notifyDataSetChanged();
+        chart.setDescriptionTextSize(25f);
+        chart.setDescription("Score over time");
+        chart.getLegend().setTextSize(25f);
+        reportsViewModel.getData().observe(getViewLifecycleOwner(), new Observer<LineData>() {
+            @Override
+            public void onChanged(LineData lineData) {
+                chart.setData(lineData);
+                chart.invalidate();
+            }
+        });
+        getActivity().findViewById(R.id.new_button).setVisibility(INVISIBLE);
         return root;
+    }
+
+    @Override
+    public void onDestroy() {
+        getActivity().findViewById(R.id.new_button).setVisibility(View.VISIBLE);
+        super.onDestroy();
     }
 }
--- app/src/main/java/ir/sharif/mobile/project/ui/reports/ReportsViewModel.java
+++ app/src/main/java/ir/sharif/mobile/project/ui/reports/ReportsViewModel.java
@@ -4,9 +4,41 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.github.mikephil.charting.data.DataSet;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import ir.sharif.mobile.project.ui.repository.RepositoryHolder;
+
 public class ReportsViewModel extends ViewModel {
 
+    private MutableLiveData<LineData> data;
 
     public ReportsViewModel() {
+        data = new MutableLiveData<>();
+    }
+
+    public LiveData<LineData> getData() {
+        Map<Date, Integer> history = RepositoryHolder.getCoinRepository().getHistory();
+        List<Date> dates = new ArrayList<>(history.keySet());
+
+        Collections.sort(dates, (o1, o2) -> o1.after(o2) ? -1 : o1.equals(o2) ? 0 : 1);
+        List<String> labels = new ArrayList<>();
+        List<Entry> entries = new ArrayList<>();
+        int dataSize = Math.min(30, dates.size()-1);
+        for (int i = 0; i <= dataSize; i++) {
+            Date date = dates.get(i);
+            entries.add(new Entry(history.get(date), dataSize - i));
+            labels.add(date.getMonth() + "/" + date.getDay() + " " + date.getHours() + ":" + date.getMinutes());
+        }
+        data.setValue(new LineData(labels, new LineDataSet(entries, "score")));
+        return data;
     }
 }
